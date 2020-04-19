077bcb8c5594f8a5695f089d67a3bb92e4bc7697
Montplaisir <montplaisir@users.noreply.github.com>
2020-04-19 14:37:40 -0400

    Add base files

---
+++ src/mozaik.py
@@ -0,0 +1,26 @@
+
+import os
+from PIL import Image
+cwd = os.getcwd()
+import blur
+
+## File to be read. ##
+# Could be generalized as input on command line.
+filename = "horacio.bmp"
+
+## Name of the filter. Used for naming output file only. ##
+# Could be generalized as input on command line, and to select filter. ## 
+filtername = "blur"
+
+outfilename = filename
+outfilename = filename.replace(".","_" + filtername + ".")
+fullfilename = cwd + '\\' + filename
+fulloutfilename = cwd + '\\' + outfilename 
+picref = Image.open(fullfilename)
+
+#pixref = picref.load()
+
+picnew = blur.blur(picref)
+
+picnew.show()
+picnew.save(fulloutfilename)

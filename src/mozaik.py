57220cbb685ea276cb7a81f897e0acc361d36496
Montplaisir <montplaisir@users.noreply.github.com>
2020-04-19 15:30:56 -0400

    Update. now it is basically working.

--- src/mozaik.py
+++ src/mozaik.py
@@ -4,9 +4,10 @@ import sys
 from PIL import Image
 cwd = os.getcwd()
 import blur
+import palette8
 
 if len(sys.argv) < 3:
-	print("Syntax: " + sys.argv[0] + " filename [blur]")
+	print("Syntax: " + sys.argv[0] + " filename [blur|palette8]")
 	exit(0)
 	
 ## File to be read. ##
@@ -22,7 +23,16 @@ fullfilename = cwd + '\\' + filename
 fulloutfilename = cwd + '\\' + outfilename 
 picref = Image.open(fullfilename)
 
-picnew = blur.blur(picref)
+def applyFilter(filtername, picref):
+	if "blur" == filtername:
+		return blur.blur(picref)
+	elif "palette8" == filtername:
+		return palette8.palette8(picref)
+	else:
+		return "Invalid filter name"
+	
+	
+picnew = applyFilter(filtername, picref)
 
 picnew.show()
 picnew.save(fulloutfilename)
--- src/palette8.py
+++ src/palette8.py
@@ -1,76 +1,93 @@
 
 #Divide in 8 colors: mean of r < 128, mean of r >= 128, idem for g and b.
 # This will be probably grayish, but it's a start.
-def palette8(picnew):
-	meanr1 = 0
-	meanr2 = 0
-	meang1 = 0
-	meang2 = 0
-	meanb1 = 0
-	meanb2 = 0
-	nr1 = 0
-	nr2 = 0
-	ng1 = 0
-	ng2 = 0
-	nb1 = 0
-	nb2 = 0
-	for i in range(width):
-		for j in range(height):
-			(r,g,b) = pixref[i,j]
-			if (r < 128):
-				meanr1 += r
-				nr1 += 1
-			else:
-				meanr2 += r
-				nr2 += 1
-			if (g < 128):
-				meang1 += g
-				ng1 += 1
-			else:
-				meang2 += g
-				ng2 += 1
-			if (b < 128):
-				meanb1 += b
-				nb1 += 1
-			else:
-				meanb2 += b
-				nb2 += 1
-	if (nr1 > 0):
-		meanr1 = int(meanr1/nr1)
-	if (ng1 > 0):
-		meang1 = int(meang1/ng1)
-	if (nb1 > 0):
-		meanb1 = int(meanb1/nb1)
-	if (nr2 > 0):
-		meanr2 = int(meanr2/nr2)
-	if (ng2 > 0):
-		meang2 = int(meang2/ng2)
-	if (nb2 > 0):
-		meanb2 = int(meanb2/nb2)
-	print(meanr1,meang1,meanb1)
-	print(meanr2,meang2,meanb2)
+
+class Palette8:
+	def __init__(self, picref):
+		self._width = picref.size[0]
+		self._height = picref.size[1]
+		self._picref = picref
+		self._pixref = picref.load()
+		
+	def getWidth(self):
+		return self._width
+
+	def getHeight(self):
+		return self._height
+		
+	def computePalette(self):
+		picnew = self._picref
+		meanr1 = 0
+		meanr2 = 0
+		meang1 = 0
+		meang2 = 0
+		meanb1 = 0
+		meanb2 = 0
+		nr1 = 0
+		nr2 = 0
+		ng1 = 0
+		ng2 = 0
+		nb1 = 0
+		nb2 = 0
+		for i in range(self._width):
+			for j in range(self._height):
+				(r,g,b) = self._pixref[i,j]
+				if (r < 128):
+					meanr1 += r
+					nr1 += 1
+				else:
+					meanr2 += r
+					nr2 += 1
+				if (g < 128):
+					meang1 += g
+					ng1 += 1
+				else:
+					meang2 += g
+					ng2 += 1
+				if (b < 128):
+					meanb1 += b
+					nb1 += 1
+				else:
+					meanb2 += b
+					nb2 += 1
+		if (nr1 > 0):
+			meanr1 = int(meanr1/nr1)
+		if (ng1 > 0):
+			meang1 = int(meang1/ng1)
+		if (nb1 > 0):
+			meanb1 = int(meanb1/nb1)
+		if (nr2 > 0):
+			meanr2 = int(meanr2/nr2)
+		if (ng2 > 0):
+			meang2 = int(meang2/ng2)
+		if (nb2 > 0):
+			meanb2 = int(meanb2/nb2)
+		
+		for i in range(self._width):
+			for j in range(self._height):
+				(r,g,b) = self._pixref[i,j]
+				(rnew, gnew, bnew) = (r,g,b)
+				if (r < 128):
+					rnew = meanr1
+				else:
+					rnew = meanr2
+				if (g < 128):
+					gnew = meang1
+				else:
+					gnew = meang2
+				if (b < 128):
+					bnew = meanb1
+				else:
+					bnew = meang2
+				pixnewij = (rnew, gnew, bnew)
+				picnew.putpixel((i,j),pixnewij)
+				
+		return picnew
+
+
+### Palette8 on whole image given by picref. ###
+def palette8(picref):
+	pal8 = Palette8(picref)
+	return pal8.computePalette()
 	
-	for i in range(width):
-		for j in range(height):
-			(r,g,b) = pixref[i,j]
-			(rnew, gnew, bnew) = (r,g,b)
-			if (r < 128):
-				rnew = meanr1
-			else:
-				rnew = meanr2
-			if (g < 128):
-				gnew = meang1
-			else:
-				gnew = meang2
-			if (b < 128):
-				bnew = meanb1
-			else:
-				bnew = meang2
-			pixnewij = (rnew, gnew, bnew)
-			#print(r,g,b)
-			#print (pixnewij)
-			picnew.putpixel((i,j),pixnewij)
-
-
-
 
